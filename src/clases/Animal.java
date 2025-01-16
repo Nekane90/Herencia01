aba67e9f3742c4075848d20a98260ed78e6d289e
nekane90 <mixhardcore@hotmail.com>
2025-01-16 11:35:33 +0100

    Clase animal completa

--- src/clases/Animal.java
+++ src/clases/Animal.java
@@ -1,5 +1,51 @@
 package clases;
 
 public class Animal {
+	private int idAnimal;
+	private String nombre;
+	private boolean chip;// Indica si tiene o no chip
+	
+	
+	
+	////////////CONSTRUCTORES/////////
+	public Animal(int idAnimal, String nombre, boolean chip) {
+		super();
+		this.idAnimal = idAnimal;
+		this.nombre = nombre;
+		this.chip = chip;
+	}
+	
+	
+	public Animal() {
+		super();
+		this.idAnimal = 0;
+		this.nombre = "********";
+		this.chip = false;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+////////////to string/////////
+		@Override
+			public String toString() {
+			return "Animal [idAnimal=" + idAnimal + ", nombre=" + nombre + ", chip=" + chip + "]";
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		}
 
+
+
+	
 }
